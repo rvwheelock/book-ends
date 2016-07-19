86b709076ce8cf45cb9504107ea5203e8befb8d7
wheelck2 <wheelck2@illinois.edu>
2016-07-19 10:17:07 -0500

    wireframe for book profile

--- app/src/main/java/com/bookends/BookProfileActivity.java
+++ app/src/main/java/com/bookends/BookProfileActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class BookProfileActivity extends AppCompatActivity {
@@ -13,6 +15,8 @@ public class BookProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_book_profile);
 
         isbn = (TextView)findViewById(R.id.ibsn);
